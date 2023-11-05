5e0840587eb630cd2be61a15cfdeece387d487b9
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-05 01:40:16 +0100

    init: start core-js-numbers

---
+++ src/getRectangleArea.js
@@ -0,0 +1,16 @@
+/**
+ * Returns an area of a rectangle given by width and height.
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {number}
+ *
+ * @example:
+ *   5, 10 => 50
+ *   5, 5  => 25
+ */
+function getRectangleArea(width, height) {
+  return width * height;
+}
+
+getRectangleArea();
