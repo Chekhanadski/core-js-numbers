257fd9ee1783b282744a75f11d7bb332a452a19d
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-05 11:57:54 +0100

    feat: add getCircleCircumference.js task

---
+++ src/getCircleCircumference.js
@@ -0,0 +1,17 @@
+/**
+ * Returns a circumference of circle given by radius.
+ *
+ * @param {number} radius
+ * @return {number}
+ *
+ * @example:
+ *   5    => 31.41592653589793
+ *   3.14 => 19.729201864543903
+ *   0    => 0
+ */
+
+function getCircleCircumference(r) {
+  return 2 * Math.PI * r;
+}
+
+getCircleCircumference();
