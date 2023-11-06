03d2537e0521642df4987a5f9cb7eeb0f2d31c51
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-06 01:34:20 +0100

    feat: add getAngleBetweenVectors task

---
+++ src/getAngleBetweenVectors.js
@@ -0,0 +1,28 @@
+/**
+ * Returns an angle (in radians) between two vectors given by xi and yi,
+ * coordinates in Cartesian plane.
+ * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
+ *
+ * @param {number} x1
+ * @param {number} y1
+ * @param {number} x2
+ * @param {number} y2
+ * @return {number}
+ *
+ * @example:
+ *   (1,0) (0,1)     => π/2
+ *   (0,1) (0,-1)    => π
+ *   (0,-1) (1,0)    => π/2
+ *   (0,1) (0,1)     => 0
+ *   (0,1) (1,2)     => 0
+ */
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const cosAngle =
+    (x1 * x2 + y1 * y2) /
+    (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2));
+
+  const angle = Math.acos(cosAngle);
+  return angle;
+}
+
+getAngleBetweenVectors();
