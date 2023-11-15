abdd7649408be18067d404872ed037164505d491
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 00:56:23 +0100

    feat: solve task "isNumber"

---
+++ src/isNumber.js
@@ -0,0 +1,24 @@
+/**
+ * Returns a boolean value indicating whether the parameter is a number or not.
+ *
+ * @param {number} number
+ * @return {boolean}
+ *
+ * @example:
+ * Infinity => false
+ * NaN      => false
+ * 0        => true
+ * 'a' / 1  => false
+ * 'a'      => false
+ * 5        => true
+ * '5'      => false
+ */
+function isNumber(number) {
+  return (
+    typeof number === 'number' &&
+    !Number.isNaN(number) &&
+    Number.isFinite(number)
+  );
+}
+
+isNumber();
