55cc9930ccf0c77cfb9e2fc984bdcfdb10c0a3d6
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-15 23:54:43 +0100

    feat: solve task "numberToStringInBase"

---
+++ src/numberToStringInBase.js
@@ -0,0 +1,16 @@
+/**
+ * Returns a string representation of a number in a specified base (radix).
+ *
+ * @param {number} number
+ * @param {number} base
+ * @return {string}
+ *
+ * @example:
+ * 255, 16 => 'ff'
+ * 2, 2    => '10'
+ */
+function numberToStringInBase(number, base) {
+  return number.toString(base);
+}
+
+numberToStringInBase();
