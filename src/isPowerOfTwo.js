ca3ec81033c8deb57d778ebe404ba9dac405f405
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-15 23:46:41 +0100

    feat: solve task "isPowerOfTwo"

---
+++ src/isPowerOfTwo.js
@@ -0,0 +1,16 @@
+/**
+ * Returns true if the given number is a power of two, false otherwise.
+ *
+ * @param {number} num
+ * @return {boolean}
+ *
+ * @example:
+ *   4   => true
+ *   16  => true
+ *   15  => false
+ */
+function isPowerOfTwo(num) {
+  return num % 2 === 0;
+}
+
+isPowerOfTwo();
