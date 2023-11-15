8b2b2706f90e2bb655fd7111c4d125af33f0068f
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-15 23:42:30 +0100

    feat: solve task "getSumOfDigits"

---
+++ src/getSumOfDigits.js
@@ -0,0 +1,22 @@
+/**
+ * Returns the sum of the digits of a given number.
+ *
+ * @param {number} num
+ * @return {number}
+ *
+ * @example:
+ *   123 => 6  // (1+2+3)
+ *   202 => 4  // (2+0+2)
+ *   5   => 5  // 5
+ */
+function getSumOfDigits(num) {
+  const strNum = String(num);
+  let sum = 0;
+  for (const i = 0; i < num; i + 1) {
+    sum += +strNum[i];
+  }
+
+  return sum;
+}
+
+getSumOfDigits();
