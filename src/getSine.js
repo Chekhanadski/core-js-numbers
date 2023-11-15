8968677bf8909d3d16d330cc17a9687b67732435
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-15 23:51:24 +0100

    feat: solve task "getSine"

---
+++ src/getSine.js
@@ -0,0 +1,15 @@
+/**
+ * Returns the sine of a number.
+ *
+ * @param {number} num
+ * @return {number}
+ *
+ * @example:
+ *   0 => 0
+ *   Math.PI / 2 => 1
+ */
+function getSine(num) {
+  return Math.sin(num);
+}
+
+getSine();
