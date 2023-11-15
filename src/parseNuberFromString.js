166cc4610ebe9733d903befdaaae64e3109c0545
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-15 19:26:44 +0100

    feat: add 3 solved task

---
+++ src/parseNuberFromString.js
@@ -0,0 +1,16 @@
+/**
+ * Returns a number by given string representation.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example:
+ *    '100'     => 100
+ *     '37'     => 37
+ * '-525.5'     => -525.5
+ */
+function parseNumberFromString(value) {
+  return Number(value);
+}
+
+parseNumberFromString();
