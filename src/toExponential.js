20afb713f7ff64f5e1675c87053298351d69a29b
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 00:04:17 +0100

    feat: solve task "toExponential"

---
+++ src/toExponential.js
@@ -0,0 +1,15 @@
+/**
+ * Returns a string representation of a number in exponential notation.
+ *
+ * @param {number} number
+ * @param {number} fractionDigits
+ * @return {string}
+ *
+ * @example:
+ * 12345, 2    => '1.23e+4'
+ */
+function toExponential(number, fractionDigits) {
+  return Math.exp(number).toFixed(fractionDigits);
+}
+
+toExponential();
