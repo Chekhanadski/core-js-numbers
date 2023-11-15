6ca459bbf0fc00de2dd275725e3b6e8b030e4b9c
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-15 23:35:02 +0100

    feat: solve task "getSumToN"

---
+++ src/getSumToN.js
@@ -0,0 +1,21 @@
+/**
+ * Returns the sum of all numbers from 1 to n.
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   5  => 15 // (1+2+3+4+5)
+ *   10 => 55 // (1+2+3+...+10)
+ *   1  => 1
+ */
+function getSumToN(n) {
+  let sum = 0;
+  for (const i = 1; i <= n; i + 1) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+getSumToN();
