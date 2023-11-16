3bd76f842088310aa2317b8cf41d81fec5aabd3b
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 23:17:53 +0100

    feat: solve task "getMaxNumber"

---
+++ src/getMaxNumber.js
@@ -0,0 +1,17 @@
+/**
+ * Returns the largest number.
+ *
+ * @param {number} firstNumber
+ * @param {number} secondNumber
+ * @return {number}
+ *
+ * @example:
+ * 1, 2   => 2
+ * -5, -6 => -5
+ * 0, 5   => 5
+ */
+function getMaxNumber(firstNumber, secondNumber) {
+  return Math.max(firstNumber, secondNumber);
+}
+
+getMaxNumber();
