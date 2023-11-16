5e3f70f42fea3b6d7e2576c51729880813585108
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 22:13:38 +0100

    feat: solve task "isSafeInteger"

---
+++ src/isSafeInteger.js
@@ -0,0 +1,16 @@
+/**
+ * Returns whether a number is a safe integer.
+ *
+ * @param {number} number
+ * @return {boolean}
+ *
+ * @example:
+ * 10       => true
+ * 3.5      => false
+ * 2 ** 53  => false
+ */
+function isSafeInteger(number) {
+  return Number.isSafeInteger(number);
+}
+
+isSafeInteger();
