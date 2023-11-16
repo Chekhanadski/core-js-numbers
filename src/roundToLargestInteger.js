c4957472295af77a8edaa78cbde8b4642ee468b0
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 22:43:22 +0100

    feat: solve task ""roundToLargestInteger"

---
+++ src/roundToLargestInteger.js
@@ -0,0 +1,15 @@
+/**
+ * Returns the largest integer greater than or equal to a given number.
+ *
+ * @param {number} number
+ * @return {number}
+ *
+ * @example:
+ * 5.1  => 6
+ * -5.9 => -5
+ */
+function roundToLargestInteger(number) {
+  return Math.ceil(number);
+}
+
+roundToLargestInteger();
