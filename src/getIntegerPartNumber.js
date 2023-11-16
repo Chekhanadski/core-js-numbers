20559da58b624b75c82f451a615d471fdde22a5a
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 23:01:58 +0100

    feat: solve task "getIntegerPartNumber"

---
+++ src/getIntegerPartNumber.js
@@ -0,0 +1,16 @@
+/**
+ * Returns the integer part of a number by removing any fractional digits.
+ *
+ * @param {number} number
+ * @return {number}
+ *
+ * @example:
+ * 5.5  => 5
+ * 5.4  => 5
+ * -5.5 => -5
+ */
+function getIntegerPartNumber(number) {
+  return Math.trunc(number);
+}
+
+getIntegerPartNumber();
