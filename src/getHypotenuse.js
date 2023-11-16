17b420b7b28f8e599c5dae3ee75dcdc440befca2
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 23:33:25 +0100

    feat: solve task "getHypotenuse"

---
+++ src/getHypotenuse.js
@@ -0,0 +1,15 @@
+/**
+ * Returns the length of the hypotenuse of a right triangle.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ *
+ * @example:
+ * 3, 4 => 5
+ */
+function getHypotenuse(a, b) {
+  return Math.sqrt(a ** 2 + b ** 2);
+}
+
+getHypotenuse();
