ff0069f4f890c8830778af459e5b251a879f265a
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 22:19:48 +0100

    feat: solve task "roundToSmallestInteger"

---
+++ src/roundToSmallestInteger.js
@@ -0,0 +1,15 @@
+/**
+ * Returns the smallest integer less than or equal to a given number.
+ *
+ * @param {number} number
+ * @return {number}
+ *
+ * @example:
+ * 5.9  => 5
+ * -5.1 => -6
+ */
+function roundToSmallestInteger(number) {
+  return Math.floor(number);
+}
+
+roundToSmallestInteger();
