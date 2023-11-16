f03f96c255de40a5602e4096b5bb573faa05543c
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 22:52:59 +0100

    feat: solve task "roundToNearestInteger"

---
+++ src/roundToNearestInteger.js
@@ -0,0 +1,16 @@
+/**
+ * Returns the value of a number rounded to the nearest integer.
+ *
+ * @param {number} number
+ * @return {number}
+ *
+ * @example:
+ * 5.5  => 6
+ * 5.4  => 5
+ * -5.5 => -5
+ */
+ function roundToNearestInteger(number) {
+  return Math.round(number);
+}
+
+roundToNearestInteger(); 
