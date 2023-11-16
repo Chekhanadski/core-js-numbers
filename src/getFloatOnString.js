ee332b02d9a7d9b440cd05f5e2d2cf6905c6ef13
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 19:58:09 +0100

    feat: solve task "getFloatOnString"

---
+++ src/getFloatOnString.js
@@ -0,0 +1,15 @@
+/**
+ * Returns a floating point number or, if the number cannot be parsed from the argument, returns NaN.
+ *
+ * @param {string} str
+ * @return {number | NaN}
+ *
+ * @example:
+ * '4.567abcdefgh' => 4.567
+ * 'abcdefgh'      => NaN
+ */
+function getFloatOnString(str) {
+  return parseFloat(str);
+}
+
+getFloatOnString();
