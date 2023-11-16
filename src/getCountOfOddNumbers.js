0b51b9507f79b1bb114ef98b1854cf986ab56122
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-16 23:40:15 +0100

    feat: solve task "getCountOfOddNumbers"

---
+++ src/getCountOfOddNumbers.js
@@ -0,0 +1,18 @@
+/**
+ * Returns count of odd numbers from zero to the resulting number.
+ * The resulting number is taken into account.
+ *
+ * @param {number} number
+ * @return {number}
+ *
+ * @example:
+ * 4  => 2
+ * 5  => 3
+ * 10 => 5
+ * 15 => 8
+ */
+function getCountOfOddNumbers(number) {
+  return Math.ceil(number / 2);
+}
+
+getCountOfOddNumbers();
