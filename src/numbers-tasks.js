e1f7093031ff6d330f7e229e883228efea734d53
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-17 03:52:27 +0100

    refactor: function isNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -446,7 +446,7 @@ function getNumberValue(number) {
  */
 function isNumber(number) {
   return (
-    typeof number === 'boolean' &&
+    typeof number === 'booleat' &&
     !Number.isNaN(number) &&
     Number.isFinite(number)
   );
