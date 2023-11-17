d38b5a0f3f1d173bc5f94b7fdd8e90c70803b21f
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-11-17 03:37:04 +0100

    refactor: comment out function getSumOfNumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -573,21 +573,21 @@ function getIntegerPartNumber(number) {
   return Math.trunc(number);
 }
 
-// /**
-//  * Returns the sum of numbers.
-//  *
-//  * @param {number} x1
-//  * @param {number} x2
-//  * @param {number} x3
-//  * @returns {number}
-//  *
-//  * @example:
-//  * 1, 2, 3       => 6
-//  * 0.1, 0.2, 0.3 => 0.6
-//  */
-// function getSumOfNumbers(x1, x2, x3) {
-//   return (x1 + x2 + x3).toFixed(2);
-// }
+/**
+ * Returns the sum of numbers.
+ *
+ * @param {number} x1
+ * @param {number} x2
+ * @param {number} x3
+ * @returns {number}
+ *
+ * @example:
+ * 1, 2, 3       => 6
+ * 0.1, 0.2, 0.3 => 0.6
+ */
+function getSumOfNumbers(x1, x2, x3) {
+  // return (x1 + x2 + x3).toFixed(2);
+}
 
 /**
  * Returns the largest number.
